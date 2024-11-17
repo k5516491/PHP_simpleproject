3b96296f8b154bde4dd4f78a9f5d928d1721808b
Yuchi <k5516491@gmail.com>
2024-11-17 20:23:31 +0800

    刪除，穩定版

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,10 +7,8 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
-$routes->get("/article", "Article::index"); //主頁，顯示全部當前文章
+$routes->get("article", "Article::index"); //主頁，顯示全部當前文章
 $routes->post("article/create","Article::create"); //新增頁面，上傳資料
 $routes->get("article/Update/(:num)","Article::show/$1"); //顯示修改頁面
 $routes->get("article/delete","Article::Delete"); //顯示刪除頁面
 //$routes->get("/article/(:num)", "Article::show/$1");
-
-
